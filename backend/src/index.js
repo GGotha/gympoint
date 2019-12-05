e8da962fd635385a8458d1fc085cd34d26b9e50c
Gotha <snakygamer@gmail.com>
2019-12-04 22:52:38 -0300

    feat: Initializing main structure

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-const server = require("./server");
-const dotenv = require("dotenv");
-dotenv.config();
-
-server.listen(process.env.DB_PORT || 4444);
-
-console.log("Servidor aberto na porta", process.env.DB_PORT);
