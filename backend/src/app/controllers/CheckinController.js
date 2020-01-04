46316e84709ef948608b7135b0f3871d53a8a8a9
Gotha <snakygamer@gmail.com>
2020-01-04 17:36:38 -0300

    fix: Arruma o find dos checkins

--- backend/src/app/controllers/CheckinController.js
+++ backend/src/app/controllers/CheckinController.js
@@ -1,5 +1,7 @@
 const { Students, Checkins } = require("../models");
-const { addDays, format, parseISO } = require("date-fns");
+const { subDays, startOfDay, endOfDay } = require("date-fns");
+
+const { Op } = require("sequelize");
 
 class CheckinController {
   async store(req, res) {
@@ -8,47 +10,29 @@ class CheckinController {
     try {
       const findForStore = await Students.findOne({ where: { id } });
 
-      if (findForStore === null) {
+      if (!findForStore) {
         return res.send({
           status: "error",
           msg: "Não foi possível encontrar um registro com esse id"
         });
       }
 
+      const dataAtual = new Date();
+
+      const dataInicio = subDays(today, 7);
+
       const findCheckin = await Checkins.findAll({
-        where: { student_id: id },
+        where: {
+          student_id: id,
+          created_at: {
+            [Op.between]: [startOfDay(dataInicio), endOfDay(dataAtual)]
+          }
+        },
+
         raw: true
       });
 
-      if (findCheckin.length >= 1) {
-        const dataDaqui7Dias = addDays(findCheckin[0].createdAt, 7);
-
-        let sql = `
-          SELECT * FROM checkins WHERE student_id=$3 AND created_at BETWEEN $1 AND $2
-      `;
-
-        var data = await Checkins.sequelize.query(sql, {
-          bind: [findCheckin[0].createdAt, dataDaqui7Dias, id],
-          raw: true,
-          type: Checkins.sequelize.QueryTypes.SELECT
-        });
-      }
-
-      //Primeiro checkin do estudante
-
-      if (data === undefined) {
-        const checkinCreate = await Checkins.create({
-          student_id: findForStore.id
-        });
-
-        return res.send({
-          status: "success",
-          msg: "Checkin realizado com sucesso!",
-          checkinCreate
-        });
-      }
-
-      if (data.length >= 5) {
+      if (findCheckin.length >= 5) {
         return res.send({
           status: "error",
           msg:
@@ -57,7 +41,7 @@ class CheckinController {
       }
 
       const checkinCreate = await Checkins.create({
-        student_id: findForStore.id
+        student_id: id
       });
 
       return res.send({
